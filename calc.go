a2ac92e5a40aca900daef65d96850bf70a85766f
Semyon Dunaev <dunaevsemyon@gmail.com>
2022-12-04 02:07:09 +0500

    add general functions

---
+++ calc.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello calc!")
+}
+
+func addition(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func division(a int, b int) int {
+	return a / b
+}
